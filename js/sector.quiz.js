c3c146079bb860cf8bf2f7b3a7e1148785447cc5
Daniel Veza <daniel@sparksinteractive.co.nz>
2019-11-05 10:10:59 +1300

    Show/Hide the questions and answers on button vlick

--- js/sector.quiz.js
+++ js/sector.quiz.js
@@ -1,10 +1,12 @@
 (function ($) {
   Drupal.behaviors.sector_media_gallery = {
     attach: function (context) {
+      $('.js-feedback-wrapper').hide();
       // TODO - clean this up after next round of sytlez.
-      $('.js-quiz-answers input[name="quiz-answer"]').on('change', function() {
+      $('.js-quiz-answers li.quiz__answer').on('click', function() {
+        $(this).next('.js-feedback-wrapper').show("fast");
+        $('.js-quiz-answers li.quiz__answer').hide("fast");
         if(!$(this).parents('.form-type-radio').hasClass('is-correct')) {
-
           $('.js-quiz-answers .form-type-radio').removeClass('is-incorrect');
           $('.js-quiz-answers .form-type-radio').next('.note--default').hide();
           $(this).parents('.form-type-radio').addClass('is-incorrect');
@@ -17,7 +19,10 @@
           $('.js-quiz-answers .form-type-radio').next('.note--default').hide();
           $(this).parents('.form-type-radio').next('.note--default').show();
         }
-        console.log('change');
+      });
+      $('.js-quiz-answers .js-quiz-back-button').on('click', function() {
+        $('.js-feedback-wrapper').hide("fast");
+        $('.js-quiz-answers li.quiz__answer').show("fast");
       });
     }
   }
